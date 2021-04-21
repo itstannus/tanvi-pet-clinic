569b4f56dd858e2eabe494cd79d12e93d9c1b06d
tshinde <tshinde@CGLX-LPT-168U>
2021-04-21 11:36:15 +0530

    Initial Project Add

---
+++ src/main/java/com/itstannus/tanvipetclinic/TanviPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.itstannus.tanvipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanviPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanviPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itstannus/tanvipetclinic/TanviPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstannus.tanvipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanviPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
